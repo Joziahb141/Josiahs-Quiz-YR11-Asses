0ade55fd480808887fc528033b45a5c08c1df509
Joziahb141 <85588967+Joziahb141@users.noreply.github.com>
2021-07-04 17:20:16 +1200

    Update quiz.py
    created a function so that users can choose which answer to guess without it breaking the program

--- quiz.py
+++ quiz.py
@@ -2,6 +2,18 @@ import requests
 from html import unescape
 import random
 
+# a function to get a int imput from user between 2 numbers - needs a message and error message lowest int value and highest int value
+def get_int_input(message, error, low, high):
+  while True:
+    try:
+      inpt = int(input(message))
+      if low < inpt < high or low == inpt or high == inpt:
+        return inpt
+      else:
+        print(error)
+    except ValueError:
+      print(error)
+      
 # gets 10 multichoice any difficulty vehicle questions and the possible answers plus more from open trivia DB using requests.get and stores it all using the variable data
 response = requests.get("https://opentdb.com/api.php?amount=10&category=28&type=multiple")
 data = response.json()['results']
@@ -25,14 +37,22 @@ for i in range (len(questions)):
   print(questions[i])
   if correct_ans_number == 1:
     print(f"1. {right_answers[i]}\n2. {wrong_answers[(i+1)*3-3]}\n3. {wrong_answers[(i+1)*3-2]}\n4. {wrong_answers[(i+1)*3-1]}")
-    # prints all the answers for the question with one being the correct one
+    users_choice = get_int_input('type in 1,2,3 or 4 to select your answer\n', 'PLEASE TYPE IN 1,2,3 OR 4 FOR YOUR ANSWER', 1, 4)
+    print(users_choice)
+    # prints all the answers for the question with one being the correct one then gets input from user
   elif correct_ans_number == 2:
     print(f"1. {wrong_answers[(i+1)*3-3]}\n2. {right_answers[i]}\n3. {wrong_answers[(i+1)*3-2]}\n4. {wrong_answers[(i+1)*3-1]}")
-    # prints all the answers for the question with two being the correct one
+    users_choice = get_int_input('type in 1,2,3 or 4 to select your answer\n', 'PLEASE TYPE IN 1,2,3 OR 4 FOR YOUR ANSWER', 1, 4)
+    print(users_choice)
+    # prints all the answers for the question with two being the correct one then gets input from user
   elif correct_ans_number == 3:
     print(f"1. {wrong_answers[(i+1)*3-3]}\n2. {wrong_answers[(i+1)*3-2]}\n3. {right_answers[i]}\n4. {wrong_answers[(i+1)*3-1]}")
-    # prints all the answers for the question with three being the correct one  
+    users_choice = get_int_input('type in 1,2,3 or 4 to select your answer\n', 'PLEASE TYPE IN 1,2,3 OR 4 FOR YOUR ANSWER', 1, 4)
+    print(users_choice)
+    # prints all the answers for the question with three being the correct one then gets input from user
   else:
     print(f"1. {wrong_answers[(i+1)*3-3]}\n2. {wrong_answers[(i+1)*3-2]}\n3. {wrong_answers[(i+1)*3-1]}\n4. {right_answers[i]}")
-    # prints all the anwsers for the question with four being the correct one
+    users_choice = get_int_input('type in 1,2,3 or 4 to select your answer\n', 'PLEASE TYPE IN 1,2,3 OR 4 FOR YOUR ANSWER', 1, 4)
+    print(users_choice)
+    # prints all the anwsers for the question with four being the correct one then gets input from user
     
